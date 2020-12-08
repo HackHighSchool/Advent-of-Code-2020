99e268b424e43a8f19ab832fef32a17e3716e100
KabirSamsi <samsikabir@gmail.com>
2020-12-07 22:14:16 -0800

    Finished Day 7

--- KabirSamsi/Day2/day2.py
+++ KabirSamsi/Day2/day2.py
@@ -1,9 +1,4 @@
-#Class template for passwords. Allows program to easily evaluate password validity, and to create storable Password objects
-class Password:
-    def __init__(self, letter, occurrence_range, pwd):
-        self.letter = letter
-        self.occurrence_range = occurrence_range
-        self.pwd = pwd
+from day2password1 import Password
 
 file = open("day2passwords.txt", 'r')
 valid_passwords = []
--- KabirSamsi/Day2/day2part2.py
+++ KabirSamsi/Day2/day2part2.py
@@ -1,9 +1,4 @@
-#Class template for passwords. Allows program to easily evaluate password validity, and to create storable Password objects
-class Password:
-    def __init__(self, letter, occurrence_spots, pwd):
-        self.letter = letter
-        self.occurrence_spots = occurrence_spots
-        self.pwd = pwd
+from day2password2 import Password
 
 file = open("day2passwords.txt", 'r')
 valid_passwords = []
