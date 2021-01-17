671fa2ba4db331372cacf34f9ebf17bedd6a3d2e
LucasStevenson <45473315+LucasStevenson@users.noreply.github.com>
2021-01-17 04:44:58 -0800

    completed day10 part2

--- LucasStevenson/day10/second.py
+++ LucasStevenson/day10/second.py
@@ -2,10 +2,24 @@ with open("input.txt") as f:
     lines = f.readlines()
     lines = [int(line.rstrip()) for line in lines if line.strip()]
 lines.sort()
-print(lines)
+lines.insert(0, 0)
+
+d = {}
 
 
-def numWaysToWrite(arr):
-    pass
+def numPermutations(index):
+    # need to use memoization to shorten runtime
+    if index == len(lines) - 1:
+        return 1
 
-# not completed yet
+    if index in d:
+        return d[index]
+    count = 0
+    for i in range(index+1, len(lines)):
+        if lines[i] - lines[index] <= 3:
+            count += numPermutations(i)
+    d[index] = count
+    return count
+
+
+print(numPermutations(0))
