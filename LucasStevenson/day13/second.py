fca6a43bc6b5c0232e0dac9004e8ff77e974425e
LucasStevenson <45473315+LucasStevenson@users.noreply.github.com>
2021-07-29 18:07:26 -0700

    completed day13 part 2

--- LucasStevenson/day13/second.py
+++ LucasStevenson/day13/second.py
@@ -1,13 +1,33 @@
+#!/usr/bin/env python3
 import math
-
 with open("input.txt") as f:
-    lines = f.readlines()
-    del lines[0]
-    lines = [line.rstrip() for line in lines[0].split(",") if line.strip()]
-    print(lines)
+    busIDs = f.readlines()
+    del busIDs[0]
+    busIDs = [busID.rstrip() for busID in busIDs[0].split(",") if busID.strip()]
 
-
-for i in range(len(lines)):
-    busID = lines[i]
-    if busID == "x":
+rem = []
+for i in range(len(busIDs)):
+    if busIDs[i] == "x":
         continue
+    rem.append(int(busIDs[i])-i)
+
+busIDs = [int(x) for x in busIDs if x != "x"]
+
+def findMinX(busIDs, rem):
+    prod = math.prod(busIDs) # product of all busIDs
+    return sum([rem[index]*(prod//busID)*pow(prod//busID,-1,busID) for index, busID in enumerate(busIDs)]) % prod
+
+'''
+This function does the same thing as the one above but with an extra loop. I'm keeping it here because I feel it's easier to read and understand.
+
+def findMinX(busIDs, rem):
+    prod = math.prod(busIDs) # product of all busIDs
+    pp = [] # pp[i] = product / busIDs[i]
+    inv = [] # modular inverse of pp[i] and busIDs[i]
+    for busID in busIDs:
+        pp.append(prod//busID)
+        inv.append(pow(prod//busID, -1, busID))
+    return sum([rem[i]*pp[i]*inv[i] for i in range(len(busIDs))])%prod
+'''
+
+print(findMinX(busIDs, rem))
